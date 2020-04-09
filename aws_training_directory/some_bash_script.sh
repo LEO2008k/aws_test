9130793ea0a3bfdd85c648954ed5741aecd5eb84
Levko Kravchuk <levkokravchuk@Levkos-MacBook-Pro.local>
2020-04-10 00:40:19 +0300

    show  private server ip

--- aws_training_directory/some_bash_script.sh
+++ aws_training_directory/some_bash_script.sh
@@ -2,8 +2,8 @@
 yum -y update
 yum -y install mc vim net-tools httpd 
 echo "---------start httpd and add html page -------------"
-echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server</h1></center></body></html>" > /var/www/html/index.html
+#privateip=`curl http://169.254169.254/latest/meta-data/local-ipv4`
+echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server with private ip $privateip</h1></center></body></html>" > /var/www/html/index.html
 systemctl enable --now httpd
 echo "UserData executed on $(date)" >>/var/www/html/log.txt
-#privateip=`curl http://169.254169.254/latest/meta-data/local-ipv4`
 echo "_________finish______________"
