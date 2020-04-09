a1436459364a23f9554158acfc89ca4528699751
Levko Kravchuk <levkokravchuk@Levkos-MacBook-Pro.local>
2020-04-10 00:37:27 +0300

    add new feachure for better known with which instance you are working

--- aws_training_directory/some_bash_script.sh
+++ aws_training_directory/some_bash_script.sh
@@ -5,4 +5,5 @@ echo "---------start httpd and add html page -------------"
 echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server</h1></center></body></html>" > /var/www/html/index.html
 systemctl enable --now httpd
 echo "UserData executed on $(date)" >>/var/www/html/log.txt
+#privateip=`curl http://169.254169.254/latest/meta-data/local-ipv4`
 echo "_________finish______________"
