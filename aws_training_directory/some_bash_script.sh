be617975d34701046735a9253bdfebc57e7f920b
Levko Kravchuk <levkokravchuk@Levkos-MacBook-Pro.local>
2020-04-09 18:36:32 +0300

    add new changes

--- aws_training_directory/some_bash_script.sh
+++ aws_training_directory/some_bash_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 yum -y update
 yum -y install mc vim net-tools httpd 
+echo "---------start httpd and add html page -------------"
+echo "<html><body bgcolor=black><center><h1><p><font color=red>Levko`s  web server"</h1></body></htnl>" > /var/www/html/index.html
+systemctl enable --now httpd
