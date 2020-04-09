333a9db10259dceebe3b1e6ad197be20ded2ff8a
Levko Kravchuk <levkokravchuk@Levkos-MacBook-Pro.local>
2020-04-09 19:06:45 +0300

    add correct changes

--- aws_training_directory/some_bash_script.sh
+++ aws_training_directory/some_bash_script.sh
@@ -2,7 +2,6 @@
 yum -y update
 yum -y install mc vim net-tools httpd 
 echo "---------start httpd and add html page -------------"
-echo "<html><body bgcolor=black><center><h1><p><font color=red>Levko`s  web server"</h1></body></htnl>" > /var/www/html/index.html
+echo "<html><body bgcolor=black><center><h1><p><font color=red> Levko  web server</h1></center></body></html>" > /var/www/html/index.html
 systemctl enable --now httpd
-echo "UserData executed on $(date)" >> /var/log/httpd/install_log_httpd.txt
 echo "_________finish______________"
